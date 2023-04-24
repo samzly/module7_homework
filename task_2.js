09582fa1c217625841be73dba396d191a54fb94d
samzly <71155025+samzly@users.noreply.github.com>
2023-04-24 19:13:47 +0300

    Correct task_2

--- task_2.js
+++ task_2.js
@@ -1,6 +1,6 @@
 function checkObj(propName, obj) {
     if ((((typeof obj) == "object") || ((typeof obj) == "function")) && ((typeof propName) == "string")) {
-        return obj.hasOwnProperty(propName)
+        return (propName in obj)
     } else {
         return 'Wrong data type'
     }
